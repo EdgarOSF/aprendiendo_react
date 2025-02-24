1c8de72927f3ea0dfdff00879aaa0dd641edb5cd
edgarosf <edgaromarsf@gmail.com>
2025-02-23 21:16:39 -0600

    app hola mundo

---
+++ projects/hola-mundo/src/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => { 
+    return (
+        <article>
+            <header style={{display: 'flex', alignItems: 'center'}}>
+                <img src="https://unavatar.io/kikobeats" alt="" />
+                <div>
+                    <strong>Edgar Omar S.</strong>
+                    <span>@edgarsof</span>
+                </div>
+            </header>
+            <aside>
+
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
