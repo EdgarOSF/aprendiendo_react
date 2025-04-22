828d4ab587183eba2aa045dfe92df447c22bb6c1
edgarosf <edgaromarsf@gmail.com>
2025-04-21 23:12:58 -0600

    componetizacion

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,48 +1,55 @@
 import { useState } from "react"
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-const Square = ({children, isSelected, updateBoard, index}) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className} >
-      {children}
-    </div>
-  )
-}
+import { Square } from "./components/Square.jsx"
+import { TURNS } from "./constants.jsx"
+import { checkWinnerFrom } from "./logic/board.jsx"
+import { WinnerModal } from "./components/WinnerModal.jsx"
+import { checkEndGame } from "./logic/board.jsx"
 
 function App() {
 
-const [board, setBoard] = useState(Array(9).fill(null)) 
-const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  // null significa que no hay ganador, false significa que hay empate
+  const [winner, setWinner] = useState(null)
 
-const updateBoard = (index) => {
-  // si hay algo en la celda no se sobreescribira
-  if (board[index]) return
+  
 
-  const newBoard = [...board]
-  newBoard[index] = turn
-  setBoard(newBoard)
+  
 
-  const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-  setTurn(newTurn)
-}
+  const updateBoard = (index) => {
+    // si hay algo en la celda no se sobreescribira
+    if (board[index] || winner) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    // checamos si hay ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
 
 
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Reset Game</button>
       <section className="game">
         {
-          board.map((_, index) => {
+          board.map((square, index) => {
             return (
               <div className="cell" key={index}>
                 <span className="cell__content">
@@ -51,7 +58,7 @@ const updateBoard = (index) => {
                     index={index}
                     updateBoard={updateBoard}
                   >
-                    {board[index]}
+                    {square}
                   </Square>
                 </span>
               </div>
@@ -67,9 +74,11 @@ const updateBoard = (index) => {
           {TURNS.O}
         </Square>
       </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame} />
     </main>
   )
-  
+
 }
 
 export default App
